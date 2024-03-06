d7632fd648b25e8766dad4bfb21087eba89bad2c
KlayvemG <klayvemguik@gmail.com>
2024-03-05 23:05:09 -0300

    updates

--- SangueHeroi/index.js
+++ SangueHeroi/index.js
@@ -11,6 +11,11 @@ const carouselList2 = document.querySelector('.carousel-list2');
 const nav_prototipo = document.querySelector('#acesse')
 const nav_sobre = document.querySelector('#sobre');
 
+const botaoInstagram = document.querySelector('#botaoInstagram');
+const botaoPrototipo = document.querySelector('#botaoPrototipo');
+const botaoSaibaMais = document.querySelector('#botaoSaibaMais');
+
+
 let currentIndex = 0;
 
 function showImage(index) {
@@ -42,3 +47,19 @@ nav_prototipo.addEventListener('click', () => {
 })
 
 setInterval(nextImage, 3000); // Alterna a imagem a ada 3 segundos 
+
+function redirecionarInstagram() {
+  window.location.href = "https://www.instagram.com/sanguee_heroi/?hl=en";
+}
+
+function redirecionarProtótipo() {
+  window.location.href = "https://www.figma.com/file/IeffJhGB8PxTDoWmI33Rj6/Untitled?type=design&node-id=0%3A1&mode=design&t=LUOrt5UOdsgh0367-1";
+}
+
+function redirecionarSaibaMais() {
+  window.location.href = "https://www.google.com";
+}
+
+botaoInstagram.addEventListener("click", redirecionarInstagram);
+botaoPrototipo.addEventListener("click", redirecionarProtótipo);
+botaoSaibaMais.addEventListener("click", redirecionarSaibaMais);
